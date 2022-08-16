ec03a9cd820a751bf26604ea75477d24891240c8
Kat Fagg <katjfagg@gmail.com>
2022-08-16 15:18:44 +0100

    start

---
+++ src/main/java/Circle.java
@@ -0,0 +1,21 @@
+public class Circle {
+
+    double radius;
+
+//    Setter
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+//    Getter
+    public double getCircumference(){
+        return radius;
+    }
+
+    public double toString() {
+    return "Circle{"
+    }
+
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        Circle
+        Circle c1 = new Circle();
+        c1.setRadius(2);
+        System.out.println(c1.getCircumference());
+    }
+
+}
